d9a0934cbbd948fb7adf90d0b84a274622111b08
toannt@hblab.vn <Truongtoan1510>
2022-05-17 16:16:08 +0700

    Init project

---
+++ renderer/pages/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <img src="/nuxt.png" />
+    <h2>Hello Nuxtron.</h2>
+    <p>Loaded from the {{ name }}</p>
+    <div>
+      <div>
+        <label> Username </label>
+        <input v-model="auth.user_name" type="text" />
+      </div>
+      <div>
+        <label> Password </label>
+        <input v-model="auth.password" type="password" />
+      </div>
+      <button type="submit" @click="login()">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ req }) {
+    return {
+      name: process.static ? "static" : process.server ? "server" : "client",
+      auth: {
+        user_name: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      if (this.auth.user_name == "123123" && this.auth.password == "123123") {
+        this.$router.push({ name: "profilePrint" });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: white;
+  color: black;
+  font-family: "Lucida Console", Monaco, monospace;
+  padding-top: 100px;
+  text-align: center;
+}
+a {
+  color: black;
+}
+</style>
---
+++ renderer/pages/profilePrint.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <img src="/nuxt.png" />
+    <h2>Profile Print Process</h2>
+    <div>
+      <div>
+        <h3>{{ status == 1 ? "Tap button for connecting device..." : "Connected!" }}</h3>
+        <button @click="connectDevice()">Connect</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ req }) {
+    return {
+      // 1: Connecting, 2. Connected
+      status: 1,
+    };
+  },
+  methods: {
+    connectDevice() {
+      this.status = 2;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: white;
+  color: black;
+  font-family: "Lucida Console", Monaco, monospace;
+  padding-top: 100px;
+  text-align: center;
+}
+a {
+  color: black;
+}
+</style>
